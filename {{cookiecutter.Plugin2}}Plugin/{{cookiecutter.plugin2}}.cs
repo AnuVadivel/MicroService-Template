b937d48e0679fcd2f6b711146160a2b54322dd8a
anu.vadivel <anu.vadivel@kotakeo.onmicrosoft.com>
2022-12-09 17:13:57 +0530

    initial commit

---
+++ {{cookiecutter.Plugin2}}Plugin/{{cookiecutter.plugin2}}.cs
@@ -0,0 +1,13 @@
+﻿using {{cookiecutter.ProjectName}}PluginBase;
+using System;
+
+namespace {{cookiecutter.Plugin2}}Plugin;
+
+public class {{cookiecutter.Plugin2}}: I{{cookiecutter.ProjectName}}Plugin
+{
+    public void MakePayment()
+    {
+        Console.WriteLine("The payment is done by visa {{cookiecutter.ProjectName}}");
+    }
+
+}
